65c2bbb4a0d4f9dcb13fc0eba4f81b857bb3a26f
Akira Abe <akiraabe2005@yahoo.co.jp>
2023-02-04 09:06:44 +0900

    modified a cheskstyle definition.

--- src/main/java/com/example/demo/MusicController.java
+++ src/main/java/com/example/demo/MusicController.java
@@ -5,25 +5,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * MusicController
+ * MusicController.
  */
 @RequestMapping("/music")
 @RestController
 public class MusicController {
 
-  /**
-   * Play music.
-   * 
-   * @return message string.
-   */
-  @GetMapping("/play")
-  public String play() {
-    return "Playing music";
-  }
+    /**
+     * Play music.
+     *
+     * @return message string.
+     */
+    @GetMapping("/play")
+    public String play() {
+        return "Playing music";
+    }
 
-  @GetMapping("/stop")
-  public String stop() {
-    return "Stopping music";
-  }
+    /**
+     * Stop music.
+     *
+     * @return message string.
+     */
+    @GetMapping("/stop")
+    public String stop() {
+        return "Stopping music";
+    }
 
 }
