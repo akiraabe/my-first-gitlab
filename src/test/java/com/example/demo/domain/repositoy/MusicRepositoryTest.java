f975e812fbc85c767c69524310f70166845568ad
Akira Abe <abe.akira@tis.co.jp>
2023-02-08 11:29:07 +0900

    Use mysql service in ci

--- src/test/java/com/example/demo/domain/repositoy/MusicRepositoryTest.java
+++ src/test/java/com/example/demo/domain/repositoy/MusicRepositoryTest.java
@@ -9,9 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.testcontainers.junit.jupiter.Testcontainers;
+//import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
+//@Testcontainers
 @DataJpaTest(excludeAutoConfiguration = AutoConfigureTestDatabase.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class MusicRepositoryTest {
